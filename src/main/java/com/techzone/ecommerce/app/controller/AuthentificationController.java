e754547b9cdeb0b9c2a8e94c9da87d884ea1a39c
ImTheOryo <enory.dhuysser@coda-student.school>
2026-01-21 13:25:22 +0100

    ALED CA MARCHE PAS

--- src/main/java/com/techzone/ecommerce/app/controller/AuthentificationController.java
+++ src/main/java/com/techzone/ecommerce/app/controller/AuthentificationController.java
@@ -1,13 +1,28 @@
 package com.techzone.ecommerce.app.controller;
 
+import com.techzone.ecommerce.shared.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthentificationController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/login")
     public String loginPage() {
         return "security/login";
+    }
+
+    @GetMapping("/login_sucess")
+    public String loginSuccess() {
+        return "security/login_succes";
+    }
+
+    @GetMapping("/errors")
+    public String errorPage() {
+        return "security/error";
     }
 }
